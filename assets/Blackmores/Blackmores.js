ce456719233d8c4c909c7e9a743c00864967ff0b
changLu <rockylc1826442304@gmail.com>
2018-05-21 13:50:38 +1000

    --

---
+++ assets/Blackmores/Blackmores.js
@@ -0,0 +1,366 @@
+const blackmores =[
+  {
+    name: "Acidophilus Bifidus",
+    pricelist: {},
+    description: "",
+    src: require('../assets/Blackmores/Acidophilus Bifidus.png')
+  },
+  {
+    name: "B12 Rapi-melt™ 1000 mcg",
+    pricelist: {},
+    description: "",
+    src: require('../assets/Blackmores/.png')
+  },
+  {
+    name: "",
+    pricelist: {},
+    description: "",
+    src: require('../assets/Blackmores/.png')
+  },
+  {
+    name: "",
+    pricelist: {},
+    description: "",
+    src: require('../assets/Blackmores/.png')
+  },
+  {
+    name: "",
+    pricelist: {},
+    description: "",
+    src: require('../assets/Blackmores/.png')
+  },
+  {
+    name: "",
+    pricelist: {},
+    description: "",
+    src: require('../assets/Blackmores/.png')
+  },
+  {
+    name: "",
+    pricelist: {},
+    description: "",
+    src: require('../assets/Blackmores/.png')
+  },
+  {
+    name: "",
+    pricelist: {},
+    description: "",
+    src: require('../assets/Blackmores/.png')
+  },
+  {
+    name: "",
+    pricelist: {},
+    description: "",
+    src: require('../assets/Blackmores/.png')
+  },
+  {
+    name: "",
+    pricelist: {},
+    description: "",
+    src: require('../assets/Blackmores/.png')
+  },
+  {
+    name: "",
+    pricelist: {},
+    description: "",
+    src: require('../assets/Blackmores/.png')
+  },
+  {
+    name: "",
+    pricelist: {},
+    description: "",
+    src: require('../assets/Blackmores/.png')
+  },
+  {
+    name: "",
+    pricelist: {},
+    description: "",
+    src: require('../assets/Blackmores/.png')
+  },
+  {
+    name: "",
+    pricelist: {},
+    description: "",
+    src: require('../assets/Blackmores/.png')
+  },
+  {
+    name: "",
+    pricelist: {},
+    description: "",
+    src: require('../assets/Blackmores/.png')
+  },
+  {
+    name: "",
+    pricelist: {},
+    description: "",
+    src: require('../assets/Blackmores/.png')
+  },
+  {
+    name: "",
+    pricelist: {},
+    description: "",
+    src: require('../assets/Blackmores/.png')
+  },
+  {
+    name: "",
+    pricelist: {},
+    description: "",
+    src: require('../assets/Blackmores/.png')
+  },
+  {
+    name: "",
+    pricelist: {},
+    description: "",
+    src: require('../assets/Blackmores/.png')
+  },
+  {
+    name: "",
+    pricelist: {},
+    description: "",
+    src: require('../assets/Blackmores/.png')
+  },
+  {
+    name: "",
+    pricelist: {},
+    description: "",
+    src: require('../assets/Blackmores/.png')
+  },
+  {
+    name: "",
+    pricelist: {},
+    description: "",
+    src: require('../assets/Blackmores/.png')
+  },
+  {
+    name: "",
+    pricelist: {},
+    description: "",
+    src: require('../assets/Blackmores/.png')
+  },
+  {
+    name: "",
+    pricelist: {},
+    description: "",
+    src: require('../assets/Blackmores/.png')
+  },
+  {
+    name: "",
+    pricelist: {},
+    description: "",
+    src: require('../assets/Blackmores/.png')
+  },
+  {
+    name: "",
+    pricelist: {},
+    description: "",
+    src: require('../assets/Blackmores/.png')
+  },
+  {
+    name: "",
+    pricelist: {},
+    description: "",
+    src: require('../assets/Blackmores/.png')
+  },
+  {
+    name: "",
+    pricelist: {},
+    description: "",
+    src: require('../assets/Blackmores/.png')
+  },
+  {
+    name: "",
+    pricelist: {},
+    description: "",
+    src: require('../assets/Blackmores/.png')
+  },
+  {
+    name: "",
+    pricelist: {},
+    description: "",
+    src: require('../assets/Blackmores/.png')
+  },
+  {
+    name: "",
+    pricelist: {},
+    description: "",
+    src: require('../assets/Blackmores/.png')
+  },
+  {
+    name: "",
+    pricelist: {},
+    description: "",
+    src: require('../assets/Blackmores/.png')
+  },
+  {
+    name: "",
+    pricelist: {},
+    description: "",
+    src: require('../assets/Blackmores/.png')
+  },
+  {
+    name: "",
+    pricelist: {},
+    description: "",
+    src: require('../assets/Blackmores/.png')
+  },
+  {
+    name: "",
+    pricelist: {},
+    description: "",
+    src: require('../assets/Blackmores/.png')
+  },
+  {
+    name: "",
+    pricelist: {},
+    description: "",
+    src: require('../assets/Blackmores/.png')
+  },
+  {
+    name: "",
+    pricelist: {},
+    description: "",
+    src: require('../assets/Blackmores/.png')
+  },
+  {
+    name: "",
+    pricelist: {},
+    description: "",
+    src: require('../assets/Blackmores/.png')
+  },
+  {
+    name: "",
+    pricelist: {},
+    description: "",
+    src: require('../assets/Blackmores/.png')
+  },
+  {
+    name: "",
+    pricelist: {},
+    description: "",
+    src: require('../assets/Blackmores/.png')
+  },
+  {
+    name: "",
+    pricelist: {},
+    description: "",
+    src: require('../assets/Blackmores/.png')
+  },
+  {
+    name: "",
+    pricelist: {},
+    description: "",
+    src: require('../assets/Blackmores/.png')
+  },
+  {
+    name: "",
+    pricelist: {},
+    description: "",
+    src: require('../assets/Blackmores/.png')
+  },
+  {
+    name: "",
+    pricelist: {},
+    description: "",
+    src: require('../assets/Blackmores/.png')
+  },
+  {
+    name: "",
+    pricelist: {},
+    description: "",
+    src: require('../assets/Blackmores/.png')
+  },
+  {
+    name: "",
+    pricelist: {},
+    description: "",
+    src: require('../assets/Blackmores/.png')
+  },
+  {
+    name: "",
+    pricelist: {},
+    description: "",
+    src: require('../assets/Blackmores/.png')
+  },
+  {
+    name: "",
+    pricelist: {},
+    description: "",
+    src: require('../assets/Blackmores/.png')
+  },
+  {
+    name: "",
+    pricelist: {},
+    description: "",
+    src: require('../assets/Blackmores/.png')
+  },
+  {
+    name: "",
+    pricelist: {},
+    description: "",
+    src: require('../assets/Blackmores/.png')
+  },
+  {
+    name: "",
+    pricelist: {},
+    description: "",
+    src: require('../assets/Blackmores/.png')
+  },
+  {
+    name: "",
+    pricelist: {},
+    description: "",
+    src: require('../assets/Blackmores/.png')
+  },
+  {
+    name: "",
+    pricelist: {},
+    description: "",
+    src: require('../assets/Blackmores/.png')
+  },
+  {
+    name: "",
+    pricelist: {},
+    description: "",
+    src: require('../assets/Blackmores/.png')
+  },
+  {
+    name: "",
+    pricelist: {},
+    description: "",
+    src: require('../assets/Blackmores/.png')
+  },
+  {
+    name: "",
+    pricelist: {},
+    description: "",
+    src: require('../assets/Blackmores/.png')
+  },
+  {
+    name: "",
+    pricelist: {},
+    description: "",
+    src: require('../assets/Blackmores/.png')
+  },
+  {
+    name: "",
+    pricelist: {},
+    description: "",
+    src: require('../assets/Blackmores/.png')
+  },
+  {
+    name: "",
+    pricelist: {},
+    description: "",
+    src: require('../assets/Blackmores/.png')
+  },
+  {
+    name: "",
+    pricelist: {},
+    description: "",
+    src: require('../assets/Blackmores/.png')
+  },
+];
+
+module.exports = {
+    blackmores: blackmores
+  };
